5e2582503e08e1229c28cc2aa7edf32ba7e71154
joedoyler <joedoyle678@gmail.com>
2020-08-31 12:56:28 +0000

    Log Out Set Up

--- app.py
+++ app.py
@@ -87,8 +87,18 @@ def profile(username):
     # grab the session user's username from db
     username = mongo.db.users.find_one(
         {"username": session["user"]})["username"]
-    return render_template("profile.html", username=username)
 
+    if session["user"]:
+        return render_template("profile.html", username=username)
+
+    return redirect(url_for("login"))
+
+
+@app.route("/logout")
+def logout():
+    # remove user from session cookies
+    flash("Have A Food Filled Day!")
+    session.pop("user")
     return redirect(url_for("login"))
 
 
