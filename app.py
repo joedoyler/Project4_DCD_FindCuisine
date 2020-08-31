edc3d0df124183b9b340aa2dfa7dbf501f8c64d1
joedoyler <joedoyle678@gmail.com>
2020-08-31 12:17:11 +0000

    Functional Log In

--- app.py
+++ app.py
@@ -25,7 +25,8 @@ def get_recipes():
     recipes = mongo.db.recipes.find()
     return render_template("recipes.html", recipes=recipes)
 
-#register page with form  
+
+# register page with form
 @app.route("/register", methods=["GET", "POST"])
 def register():
     if request.method == "POST":
@@ -44,9 +45,37 @@ def register():
 
         session["user"] = request.form.get("username").lower()
         flash("You Are Officially A Foodie!")
-        
-
     return render_template("register.html")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        # Confirms username is the same as on database
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            # check to make sure that password entered
+            #  matches password stored on database
+            if check_password_hash(
+                    existing_user["password"], request.form.get("password")):
+                        session["user"] = request.form.get("username").lower()
+                        flash("Welcome, {}".format(
+                            request.form.get("username")))
+
+            else:
+                # if password doesn't exist
+                flash("Incorrect Username and/or Password")
+                return redirect(url_for("login"))
+
+        else:
+            # if user doesn't exist
+            flash("Incorrect Username and/or Password")
+            return redirect(url_for("login"))
+
+    return render_template("login.html")
+
 
 
 if __name__ == "__main__":
